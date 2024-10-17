3b3b8daa9f39b22451b4d8318b37cf9665edcce4
Bruno-Diego <bruno.diego.x@hotmail.com>
2024-10-17 17:51:41 +0200

    Add footer basic structure and social links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/Navbar";
 
 import { ClerkProvider } from "@clerk/nextjs";
 import Script from "next/script";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,6 +32,13 @@ export default function RootLayout({
             async
             src="https://cdn-cookieyes.com/client_data/3c11a35db5e7e097d7ea4762/script.js"
           />
+          <Script
+            id="font-awesome"
+            type="text/javascript"
+            async
+            src="https://kit.fontawesome.com/df96160574.js"
+          />
+          <Footer />
         </body>
       </html>
     </ClerkProvider>
---
+++ src/components/Footer.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="border-t-2 border-red-500 w-full flex">
+      <div className="mx-auto">
+        <ul className="flex text-white">
+          <li className="m-2">
+            <div>
+              <Link target="_blank" href="https://www.instagram.com/star_turkish_pizza_kebap/">
+                <i className="fa-brands fa-instagram fa-2xl"></i>
+              </Link>
+            </div>
+          </li>
+          <li className="m-2">
+            <div>
+              <Link target="_blank" href="https://wa.me/393240560356">
+                <i className="fa-brands fa-whatsapp fa-2xl"></i>
+              </Link>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
